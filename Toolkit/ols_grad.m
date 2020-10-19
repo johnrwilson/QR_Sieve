099e069d1bdbb14fa5c945403b84f06dca12bf97
hliu-berkeley <67452472+hliu-berkeley@users.noreply.github.com>
2020-10-19 14:22:27 -0700

    Update ols_grad.m

--- Toolkit/ols_grad.m
+++ Toolkit/ols_grad.m
@@ -1,4 +1,14 @@
 function [loss, gradient] = ols_grad(b, y_dat, X_dat)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ols_grad
+%
+% Errors in the Dependent Variable of Quantile Regression Models
+%
+% Jerry Hausman, Haoyang Liu, Ye Luo, Christopher Palmer 2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
     loss = (y_dat - X_dat*b)'*(y_dat - X_dat*b) / size(y_dat,1);
     gradient = -2 * X_dat' * (y_dat - X_dat*b) / size(y_dat,1);
 end
