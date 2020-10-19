f7fe44e2223181ff80223e699eb1dfed1ed3d7ae
hliu-berkeley <67452472+hliu-berkeley@users.noreply.github.com>
2020-10-19 14:49:07 -0700

    Update sortbeta_1.m

--- Toolkit/sortbeta_1.m
+++ Toolkit/sortbeta_1.m
@@ -1,5 +1,14 @@
 function [beta_WLS_start_sorted,I_WLS_start] = sortbeta_1(X_mean,beta_WLS_start,ntau,nx)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% sortbeta_1
+% Sort knots based on the predicted values
+%
+% Errors in the Dependent Variable of Quantile Regression Models
+%
+% Jerry Hausman, Haoyang Liu, Ye Luo, Christopher Palmer 2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 fval_tau_WLS_start = X_mean*beta_WLS_start';
 [V,I_WLS_start] = sort(fval_tau_WLS_start);
 for j_x = [1:nx]
