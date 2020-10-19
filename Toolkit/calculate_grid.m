610c802e3338a0226154a8bfe85cdedc4eeb6584
hliu-berkeley <67452472+hliu-berkeley@users.noreply.github.com>
2020-10-19 13:54:29 -0700

    Update calculate_grid.m

--- Toolkit/calculate_grid.m
+++ Toolkit/calculate_grid.m
@@ -1,5 +1,14 @@
 function [taugrid_qreg, taugrid_midpoint, taugrid_ue] = calculate_grid(ntau)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% calculate_grid
+% Calculate grids for different estimators
+%
+% Errors in the Dependent Variable of Quantile Regression Models
+%
+% Jerry Hausman, Haoyang Liu, Ye Luo, Christopher Palmer 2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 taugrid_qreg = (1:ntau)/(ntau+1);
 taugrid_midpoint = (1/2*([0:(ntau-1)]  + [1:(ntau)]))/ntau;
 taugrid_ue = taugrid_midpoint;
